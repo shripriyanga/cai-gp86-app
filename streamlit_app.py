5fc061bd594389ba2f4bfb3621dd087839516257
shripriyanga <shripriyanga@users.noreply.github.com>
2025-03-16 14:40:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,8 @@ if user_input:
     st.session_state.messages.append({"role": "user", "content": user_input})
 
     # Hardcoded bot response
-    bot_response = ask_local_llm(user_input, retrieved_chunks)
+    bot_response = "Harcoded checks"
+    #bot_response = ask_local_llm(user_input, retrieved_chunks)
 
     # Add the bot's response to the chat history
     st.session_state.messages.append({"role": "bot", "content": bot_response})
